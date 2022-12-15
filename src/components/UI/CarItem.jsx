fd4bfe9bb5efa4cc619204e13fcd4a31180694ae
esterdalia <102993023+esterdalia@users.noreply.github.com>
2022-12-15 12:23:39 -0300

    Add files via upload

---
+++ src/components/UI/CarItem.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const CarItem = (props) => {
+  const { category, type, rentPrice, imgUrl, carName,city } = props.item;
+  return (
+    <div className="car__item">
+      <div className="car__item-top">
+        <div className="car__item-tile">
+          <h3>{carName}</h3>
+          <span>
+
+          </span>
+        </div>
+        <p>{category}</p>
+        <p>{city}</p>
+      
+      </div>
+
+      <div className="car__img-center">
+        <img src={imgUrl} alt="" />
+      </div>
+
+      <div className="car__item-bottom">
+        <div className="car__item-left">
+          <p>
+            {type}
+          </p>
+        </div>
+
+        <p className="car__rent">R$ {rentPrice}/Diária</p>
+      </div>
+    </div>
+  );
+};
+
+export default CarItem;
